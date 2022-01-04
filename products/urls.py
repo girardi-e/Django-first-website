7a7066ad36382b26e1d31a996c3034a8807c8ff5
emilio girardi <emilio.g.girardi@gmail.com>
2022-01-04 17:44:17 +0100

    url patterns

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 # /products
-urlspatterns = [
+urlpatterns = [
     path("", views.index)
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+# from django.shortcuts import render
 
 
+# URL (Uniform Resource Locator -> Address)
+# /products -> index
 def index(request):
     return HttpResponse("Welcome to PyShop! Feel free to browse our products")
