914e26b1738b660cd8d02256aa74d79a57f2b629
emilio girardi <emilio.g.girardi@gmail.com>
2022-01-05 15:46:54 +0100

    offer model and migration

--- products/models.py
+++ products/models.py
@@ -7,3 +7,10 @@ class Product(models.Model):
     price = models.FloatField()
     stock = models.IntegerField()
     image_url = models.CharField(max_length=2083)
+
+
+# Offer model and attributes
+class Offer(models.Model):
+    code = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
