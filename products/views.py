20f88bf8e637eede222373580bc7c11a6405fb97
emilio girardi <emilio.g.girardi@gmail.com>
2022-01-06 13:31:43 +0100

    products template

--- products/views.py
+++ products/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render
 # /products -> index
 def index(request):
     products = Product.objects.all()
-    return render(request, "index.html")
+    return render(request, "index.html", {"products": products})
 
 
 # /products/new
