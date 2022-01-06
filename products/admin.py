fee2a152e08b2a0a25bdfa9e53d601bec3376368
emilio girardi <emilio.g.girardi@gmail.com>
2022-01-06 12:43:39 +0100

    products admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Product
 
+
 # manage Product in the admin area
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "stock")
+
+
+admin.site.register(Product, ProductAdmin)
